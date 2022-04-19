febfc778a61aaa861c54d0075c36aae10ef92940
DESKTOP-LS6HTL8\Michał <236616@edu.p.lodz.pl>
2022-04-19 15:55:58 +0200

    Etap1 - Data

---
+++ Bank/Ball.cs
@@ -0,0 +1,29 @@
+﻿namespace Data
+{
+    public class Ball
+    {
+        private readonly int _ballID;
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public int XMove { get; set; }
+        public int YMove { get; set; }
+        public int Radius { get; set; }
+        public string color { get; }
+
+        public Ball(int ID, int xPos, int yPos, int radius, int xMov, int yMov)
+        {
+            _ballID = ID;
+            XPos = xPos;
+            YPos = yPos;
+            XMove = xMov;
+            YMove = yMov;
+            Radius = radius;
+            color = Color.PickColor();
+        }
+
+        public int GetID()
+        {
+            return _ballID;
+        }
+    }
+}
