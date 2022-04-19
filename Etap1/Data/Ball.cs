9cb83d4aed539ec467eabe81c950ff04b4d6af42
236616 Oczkowski Michal <236616@edu.p.lodz.pl>
2022-04-19 15:47:49 +0200

    Etap1 - Data

---
+++ Etap1/Data/Ball.cs
@@ -0,0 +1,29 @@
+namespace Data
+{
+    public class Ball
+    {
+        private readonly int _ballID;
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public int XMove { get; set; }
+        public int Ymove { get; set; }
+        public int Radius { get; set; }
+        public string color { get; }
+
+        public Ball(int ID, int xPos, int yPos, int radius, int xMov, int yMov)
+        {
+            _ballID = ID;
+            XPos = xPos;
+            YPos = yPos;
+            XMove = xMov;
+            Ymove = yMov;
+            Radius = radius;
+            color = Color.PickColor();
+        }
+
+        public int GetID()
+        {
+            return _ballID;
+        }
+    }
+}
