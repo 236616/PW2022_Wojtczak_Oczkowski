e752987eb443cccd3b76b770970d84fbf7a4295b
DESKTOP-LS6HTL8\Michał <236616@edu.p.lodz.pl>
2022-04-20 19:20:21 +0200

    Model + dependents

---
+++ Model/MainMap.cs
@@ -0,0 +1,39 @@
+﻿using Data;
+using LogicLayer;
+
+namespace Presentation.Model
+{
+    public class MainMap
+    {
+        private readonly BallsManager _ballsManager;
+        private int _width;
+        private int _height;
+
+        public void Tick()
+        {
+            _ballsManager.DoTick();
+        }
+
+        public MainMap(int w, int h)
+        {
+            _width = w;
+            _height = h;
+            _ballsManager = new BallsManager(_width, _height);
+        }
+
+        public List<Ball> GetBalls()
+        {
+            return _ballsManager.GetAllBalls();
+        }
+
+        public void CreateBalls(int amount)
+        {
+            _ballsManager.SummonBalls(amount);
+        }
+
+        public void ClearMap()
+        {
+            _ballsManager.ClearMap();
+        }
+    }
+}
